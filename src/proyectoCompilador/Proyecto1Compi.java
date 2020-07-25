df8b75390d76c8ade2b0764be33e8edf7851a6f8
Juanca0399 <juancaruizs@github.com>
2020-07-25 00:33:55 -0600

    solo falta el manejo de errores :)

--- src/proyectoCompilador/Proyecto1Compi.java
+++ src/proyectoCompilador/Proyecto1Compi.java
@@ -18,9 +18,10 @@ import jflex.exceptions.SilentExit;
 public class Proyecto1Compi {
     public static void main(String[] args) throws SilentExit, Exception {
         //generarJavalexer();                               //-----> genera el archivo Scanner.java
+        //System.out.println("hola");
         //generarJavaparser();                              //-----> genera los archivos parser.java y sym.java
         try {
-            String ST = new String(Files.readAllBytes(Paths.get("src/proyectoCompilador/PruebaVariables.txt")));
+            String ST = new String(Files.readAllBytes(Paths.get("src/proyectoCompilador/PruebaExpresiones.txt")));
             System.out.print(ST);
             System.out.print("\n");
             System.out.print("\n");
